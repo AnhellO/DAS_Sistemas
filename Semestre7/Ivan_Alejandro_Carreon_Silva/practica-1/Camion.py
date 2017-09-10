ef0ef5ecc9c03d2165a3a3287402082bd24f0b2d
icarreons <akatsuki96_ivan@hotmail.com>
2017-09-10 13:02:44 -0500

    parte 3,me faltaron algunas cosas xc

--- Semestre7/Ivan_Alejandro_Carreon_Silva/practica-1/Camion.py
+++ Semestre7/Ivan_Alejandro_Carreon_Silva/practica-1/Camion.py
@@ -2,16 +2,19 @@ from Vehiculo import Vehiculo
 
 class Camion(Vehiculo):
     def __init__(self,marca,modelo,color,transmision,cilindros,precio,motor,sku,existencia,ejes,potencia):
-        super().__init__(marca,modelo,color,transmision,cilindros,precio,motor,sku)
+        super().__init__(marca,modelo,color,transmision,cilindros,precio,motor,sku,existencia)
         self.ejes = ejes
         self.potencia = potencia
 
     def setEjes(self,ejes):
         self.ejes = ejes
-    def getEjes(Self):
+    def getEjes(self):
         return self.ejes
 
     def setPotencia(self,potencia):
         self.potencia = potencia
     def getPotencia(self):
         return self.potencia
+
+camion1 = Camion('Mercedez Benz','Zetros','Rojo','OM 926 LA','6','113.146 €','Actros 12.0 V6 de 320 caballos','0300','3','2ejes','240 kW (326 CV)')
+camion2 = Camion('Scania ','P280','Blanco','Allison 4500PR,6 Speed Automatic ','6','$2,133,820','Euro 5 280 CV','0301','2','1eje','280 CV')
--- Semestre7/Ivan_Alejandro_Carreon_Silva/practica-1/main.py
+++ Semestre7/Ivan_Alejandro_Carreon_Silva/practica-1/main.py
@@ -4,7 +4,6 @@ from Agencia import *
 from Automovil import *
 from Camion import *
 from Motocicleta import *
-import webbrowser
 
 i = Vendedor1.getNombre()+" "+Vendedor1.getApellidoPaterno()+" "+Vendedor1.getApellidoMaterno()+" "+Vendedor1.getSexo()+" "+Vendedor1.getEdad()+" "+Vendedor1.getDomicilio()+" "+Vendedor1.getTelefono()+" "+Vendedor1.getIDVendedor()
 i2 = Vendedor2.getNombre()+" "+Vendedor2.getApellidoPaterno()+" "+Vendedor2.getApellidoMaterno()+" "+Vendedor2.getSexo()+" "+Vendedor2.getEdad()+" "+Vendedor2.getDomicilio()+" "+Vendedor2.getTelefono()+" "+Vendedor2.getIDVendedor()
@@ -23,6 +22,9 @@ ca3 = Carro3.getMarca()+" "+Carro3.getModelo()+" - "+Carro3.getColor()+" - "+Car
 mo = Moto1.getMarca()+" "+Moto1.getModelo()+" - "+Moto1.getColor()+" - "+Moto1.getTransmision()+" - "+Moto1.getCilindros()+" - "+Moto1.getPrecio()+" - "+Moto1.getMotor()+" - "+Moto1.getTipo()+" - "+Moto1.getCenCub()
 mo2 = Moto2.getMarca()+" "+Moto2.getModelo()+" - "+Moto2.getColor()+" - "+Moto2.getTransmision()+" - "+Moto2.getCilindros()+" - "+Moto2.getPrecio()+" - "+Moto2.getMotor()+" - "+Moto2.getTipo()+" - "+Moto2.getCenCub()
 mo3 = Moto3.getMarca()+" "+Moto3.getModelo()+" - "+Moto3.getColor()+" - "+Moto3.getTransmision()+" - "+Moto3.getCilindros()+" - "+Moto3.getPrecio()+" - "+Moto3.getMotor()+" - "+Moto3.getTipo()+" - "+Moto3.getCenCub()
+
+cam = camion1.getMarca()+" "+camion1.getModelo()+" - "+camion1.getColor()+" - "+camion1.getTransmision()+" - "+camion1.getCilindros()+" - "+camion1.getPrecio()+" - "+camion1.getMotor()+" - "+camion1.getEjes()+" - "+camion1.getPotencia()
+cam2 = camion2.getMarca()+" "+camion2.getModelo()+" - "+camion2.getColor()+" - "+camion2.getTransmision()+" - "+camion2.getCilindros()+" - "+camion2.getPrecio()+" - "+camion2.getMotor()+" - "+camion2.getEjes()+" - "+camion2.getPotencia()
 
 opcion = 7
 
@@ -52,13 +54,21 @@ while (opcion != 0):
         print(mo + "\n" + mo2 + "\n" + mo3)
         print("=" * 80)
         print('Camiones \n')
-
+        print(cam + "\n" + cam2)
+        print("=" * 80)
     elif gato == 2:
         vv = Sagencia.ventaVehiculo( (Cliente2.getNombre()+" "+Cliente2.getApellidoPaterno()+" "+Cliente2.getApellidoMaterno()+"\n" ),(Carro1.getMarca()+" "+Carro1.getModelo()+"\n" ),(Vendedor1.getNombre() )+" "+(Vendedor1.getApellidoPaterno()+" "+Vendedor1.getApellidoMaterno() ) )
         print(vv)
-        print('=' * 80)
+        print("=" * 80)
+        cc = Sagencia.ventaVehiculo( (Cliente3.getNombre()+" "+Cliente3.getApellidoPaterno()+" "+Cliente3.getApellidoMaterno()+"\n" ),(camion1.getMarca()+" "+camion1.getModelo()+"\n" ),(Vendedor2.getNombre() )+" "+(Vendedor2.getApellidoPaterno()+" "+Vendedor2.getApellidoMaterno() ) )
+        print(cc)
+        print("=" * 80)
+        mm = Sagencia.ventaVehiculo( (Cliente1.getNombre()+" "+Cliente1.getApellidoPaterno()+" "+Cliente1.getApellidoMaterno()+"\n" ),(Moto3.getMarca()+" "+Moto3.getModelo()+"\n" ),(Vendedor3.getNombre() )+" "+(Vendedor3.getApellidoPaterno()+" "+Vendedor3.getApellidoMaterno() ) )
+        print(mm)
+        print("=" * 80)
     elif gato == 3:
-        webbrowser.open(menu.gatoGifRandom())
+        print('Estamos trabajando en eso :v')
+        print("=" * 80)
     elif gato == 4:
         print(cl +"\n"+ cl2 +"\n"+ cl3)
         print("=" *80)
