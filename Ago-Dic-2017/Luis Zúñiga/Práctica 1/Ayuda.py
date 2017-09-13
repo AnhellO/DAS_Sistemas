89578f173f074f2795f0c64a98d49b75846389d2
fanborges <fanborges@gmail.com>
2017-09-13 01:55:13 -0500

    cambios menores en el archivo de Ayuda

--- Ago-Dic-2017/Luis Zúñiga/Práctica 1/Ayuda.py
+++ Ago-Dic-2017/Luis Zúñiga/Práctica 1/Ayuda.py
@@ -1,11 +1,11 @@
 class Ayuda:
     def imprimeAyuda(self):
         print('='*80)
-        print('''\
+        print('''
         CONOCER SOBRE:
 1.Agregar/Eliminar (Clientes, Empleados,Productos).
 2.Consultar.
-3.Compraventa.\
+3.Compraventa.
         ''')
         print('='*80)
         ayudin = int(input('help>> '))
@@ -20,35 +20,35 @@ class Ayuda:
             
     def imprimeAyudaAgregar(self):
         print('='*80)       
-        print('''\
-            AGREGAR EMPLEADOS, CLIENTES O PRODUCTOS.
+        print('''
+        AGREGAR EMPLEADOS, CLIENTES O PRODUCTOS.
             
-<<comando>> [\'argumentos\'] Nota: Todos los argumentos son obligatorios.
+<<comando>> ['argumentos'] Nota: Todos los argumentos son obligatorios.
 
-Ej: empleado Cosme Fulanito Pérez 40 masculino \'Av. Siempreviva #100\'
+Ej: empleado Cosme Fulanito Pérez 40 masculino 'Av. Siempreviva #100'
              555-666-777         
              
-<<empleado>> [\'nombre\' \'ap. paterno\' \'ap. materno\' \'edad\' \'género\'
-              \'dirección\' \'teléfono\']
+<<empleado>> ['nombre' 'ap. paterno' 'ap. materno' 'edad' 'género'
+              'dirección' 'teléfono']
               Agrega un empleado al sistema.
 
-<<cliente>> [\'nombre\' \'ap. paterno\' \'ap. materno\' \'edad\' \'género\'
-             \'dirección\' \'teléfono\']
+<<cliente>> ['nombre' 'ap. paterno' 'ap. materno' 'edad' 'género'
+             'dirección' 'teléfono']
              Agrega un cliente al sistema       
 
 <<vehiculo>>
-           <<camion>> [\'marca\' \'modelo\' \'color\' \'motor\' \'trans.\'
-                        \'ejes\' \'potencia\' \'capacidad\' \'cantidad\' 
-                        \'precio\']       
+           <<camion>> ['marca' 'modelo' 'color' 'motor' 'trans.'
+                        'ejes' 'potencia' 'capacidad' 'cantidad' 
+                        'precio']       
                        Agrega un camión al sistema.
            
-           <<auto>> [\'marca\' \'modelo\' \'color\' \'motor\' \'trans.\'
-                     \'puertas\' \'equipado <si> <no>\' \'km/l\' \'cantidad\'
-                     \'precio\']
+           <<auto>> ['marca' 'modelo' 'color' 'motor' 'trans.'
+                     'puertas' 'equipado <si> <no>' 'km/l' 'cantidad'
+                     'precio']
                      Agrega un automóvil al sistema.
             
-           <<moto>> [\'marca\' \'modelo\' \'color\' \'motor\' \'trans.\' \'cc\'
-                      \'cantidad\' \'precio\']'
+           <<moto>> ['marca' 'modelo' 'color' 'motor' 'trans.' 'cc'
+                      'cantidad' 'precio']
                      Agrega una motocicleta al sistema.
         ''')
         print('='*80)
@@ -56,14 +56,16 @@ Ej: empleado Cosme Fulanito Pérez 40 masculino \'Av. Siempreviva #100\'
     
     def imprimeAyudaConsultar(self):
         print('='*80)
-        print('''\
+        print('''
         CONSULTAR EMPLEADOS, CLIENTES O PRODUCTOS.
-<<comando>> [\'argumentos\'] Nota: Todos los argumentos son obligatorios.
+        
+<<comando>> ['argumentos'] Nota: Todos los argumentos son obligatorios.
+
 <<consulta>>
         <<clientes>> arroja una lista de los clientes con su respectiva
                      información.        
         
-        <<cliente>> [\'numero de cliente\']: arroja la información de un
+        <<cliente>> ['numero de cliente']: arroja la información de un
                     cliente específico.
         
         <<autos>> arroja una lista de los automóviles con su respectiva 
@@ -80,16 +82,18 @@ Ej: empleado Cosme Fulanito Pérez 40 masculino \'Av. Siempreviva #100\'
     def imprimeAyudaVenta(self):
         print('='*80)
         print('''
+        
         VENTAS.
-<<comando>> [\'argumentos\'] Nota: Todos los argumentos son obligatorios.
+                                
+<<comando>> ['argumentos'] Nota: Todos los argumentos son obligatorios.
 <<venta>>
-        <<camion>> [\'numero de vendedor\' \'numero de cliente\' \'SKU\']
+        <<camion>> ['numero de vendedor' 'numero de cliente' 'SKU']
                      Vende un camión.
                      
-        <<auto>> [\'numero de vendedor\' \'numero de cliente\' \'SKU\']
+        <<auto>> ['numero de vendedor' 'numero de cliente' 'SKU']
                    Vende un auto.
                    
-        <<moto>> [\'numero de vendedor\' \'numero de cliente\' \'SKU\']
+        <<moto>> ['numero de vendedor' 'numero de cliente' 'SKU']
                    Vende una motocicleta.
         ''')
         print('='*80)
