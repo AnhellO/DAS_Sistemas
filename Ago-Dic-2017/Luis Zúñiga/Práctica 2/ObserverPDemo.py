1cfaac107f370860ce469b905b8b9a2dcb18d50b
fanborges <fanborges@gmail.com>
2017-09-26 04:35:00 -0500

    archivos práctica 2

---
+++ Ago-Dic-2017/Luis Zúñiga/Práctica 2/ObserverPDemo.py
@@ -0,0 +1,24 @@
+import time
+import random
+from Observer import Observer
+import os
+from Subject import Subject
+from BinaryObserver import BinaryObserver
+from OctalObserver import OctalObserver
+from HexObserver import HexObserver
+
+sujeto = Subject
+
+observador = Observer(sujeto)
+observador = BinaryObserver(sujeto)
+observador = OctalObserver(sujeto)
+observador = HexObserver(sujeto)
+
+for x in range(20, 0, -1):
+    os.system('cls')
+    r = random.randint(0,100)
+    sujeto.setState(sujeto,r)    
+    time.sleep(2)
+
+
+
