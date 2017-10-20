15ab9114c16594e4bf5389d7df1ff97d00059280
CMAREQ <temomareq@gmail.com>
2017-10-19 20:19:15 -0500

    Prueba en Branch Holis :D

--- Ago-Dic-2017/Cuauhtémoc Martínez/Holis/hola.py
+++ Ago-Dic-2017/Cuauhtémoc Martínez/Holis/hola.py
@@ -4,7 +4,7 @@ def printHola():
     lista = ['Mundo', 'Hola', 'Guapo', 'Bola', 'Calamardo']
 
     for x in range(10):
-        r = random.randint(0,5)
+        r = random.randint(0,4)
         print ('Hola', lista[r])
 
 
