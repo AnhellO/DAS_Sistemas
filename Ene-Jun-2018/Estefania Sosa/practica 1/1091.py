14a69c7c7221a4c5cf79c7ae303464aed63f7060
estefan17e <sombra17e@gmail.com>
2018-03-15 01:56:36 -0600

    1 parcial

--- Ene-Jun-2018/Estefania Sosa/practica 1/1091.py
+++ Ene-Jun-2018/Estefania Sosa/practica 1/1091.py
@@ -1,20 +1,20 @@
 import sys
 sys.stdin
-while 1:
+k=1
+while k>0 and k<=1000:
 	k=int(input())
-	if k>0 and k<=1000:
-		N,M=list(map(int, input().split(' ')))
-		if N>-10000 or M<10000:
-			for x in range(k):
-				X,Y=list(map(int, input().split(' ')))
-				if X>=-10000 or Y<=10000:
-					if X>N and Y>M:
-						print("NE")
-					elif X<N and Y>M:
-						print("NO")
-					elif X<N and Y<M:
-						print("SO")
-					elif X>N and Y<M:
-						print("SE")
-					elif X==N or Y==M:
-						print("divisa")
+	N,M=list(map(int, input().split(' ')))
+	if N>=-10000 or M<=10000:
+		for x in range(k):
+			X,Y=list(map(int, input().split(' ')))
+			if X>=-10000 or Y<=10000:
+				if X>N and Y>M:
+					print("NE")
+				elif X<N and Y>M:
+					print("NO")
+				elif X<N and Y<M:
+					print("SO")
+				elif X>N and Y<M:
+					print("SE")
+				elif X==N or Y==M:
+					print("divisa")
