5444c4bd25944f6e737c9f6f6f4d2a9f606e8306
Angel Santiago Jaime Zavala <anhell.death999@gmail.com>
2018-05-16 21:04:33 -0500

    Agregando ejemplo ORM

---
+++ Ene-Jun-2018/Practicas/Ordinario/orm-ejemplos.py
@@ -0,0 +1,26 @@
+from peewee import *
+
+db = SqliteDatabase('DAS.db')
+
+class Usuarios(Model):
+    id = IntegerField()
+    nombre = TextField()
+    edad = IntegerField()
+
+    class Meta:
+        database = db # This model uses the "people.db" database.
+
+    def __str__(self):
+        return "Nombre:{}\nEdad:{}\n".format(self.nombre, self.edad)
+
+sujeto = Usuarios.get(Usuarios.id == 666)
+print(sujeto)
+
+sujeto.edad = 18
+sujeto.save()
+print(sujeto)
+
+#sujeto2 = Usuarios.create(id=9818, nombre='Claudia', edad=18)
+
+for usuario in Usuarios.select():
+    print(usuario)
