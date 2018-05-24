e2b5c1fbe98342421ace7786b2174eabd412d6d0
Luis Carielo <lcarielo@gmail.com>
2018-05-23 20:42:17 -0500

    Single

---
+++ Ene-Jun-2018/Luis Carielo/Segundo Parcial/reporte.py
@@ -0,0 +1,60 @@
+import json
+
+class Report:
+
+    def getTitle(self):
+        return 'Title!'
+
+    def getDate(self):
+        return '2018-05-23'
+
+    def getContents(self):
+        return {
+        	'title': self.getTitle(),
+        	'date': self.getDate()
+        }
+
+    def formatJson(self):
+    	return json.dumps(self.getContents())
+
+report = Report()
+print(report.getContents())
+
+
+###################################################################################################
+###################################################################################################
+###################################################################################################
+import json
+
+class Title:
+    def getTitle(self):
+        return "Title"
+
+class Date:
+    def getDate(self):
+        return "2018-05-23"
+
+class Contents:
+    def getContents(self):
+        return {"title:" self.getTitle(), "date:" self.getDate}
+
+class Json:
+    return json.dumps(self.getContents())
+
+titulo = Title()
+print(getTitle())
+
+fecha = Date()
+print(getDate())
+
+contenido = Contents()
+print(getContents())
+
+yeison = Json()
+print(yeison)
+
+"""
+Separé las funcionalidades en clases para que las peticiones sean más sencillas de implementar y
+si llegase a existir algun crecimiento, que no haya tanto problema para implementar las nuevas
+funcionalidades
+"""
