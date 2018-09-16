c9c6e237a1b5851e0baedef3b563e00393e7fe54
Cynthia\Cynthia <cgg2709@gmail.com>
2018-09-15 19:03:11 -0500

    Añadienndo archivos faltantes Practica 2

--- Ago-Dic-2018/CynthiaGarcía/Restaurant.py
+++ Ago-Dic-2018/CynthiaGarcía/Restaurant.py
@@ -1,18 +1,28 @@
-class Restaurant:
-	def __init__(self, nombre='Felipe', cuisine_type='no se', estado=''):
-		self.nombre=nombre
+class Restaurant():
+	def __init__(self, nombre, cuisine_type):
+		self.nombre=nombre.title()
 		self.cuisine_type=cuisine_type
-		self.estado=estado
+		
 
-	def __str__(self):
-		return('Nombre:{} \nType: {}  \nestado: {}'.format(self.nombre, self.cuisine_type, self.estado))
+	def describe_restaruant(self):
+		aux="Nombre:"+ self.nombre + "\n"+"Tipo:"+ self.cuisine_type
+		print("\n" + aux)
 
 	def open_restaurant(self):
-		estado=(sel.estado)
-		print(estado)
+		aux="El" + self.nombre + "esta Abierto"
+		print("\n"+ aux) 
 
-describe_restaurant=Restaurant()
-print(describe_restaurant)
+rest=Restaurant('Servicio express', 'pizza')
+rest.describe_restaruant()
+rest.open_restaurant()
 
-open_restaurant=Restaurant('Doña','Gorditas','Abierto')
-print(open_restaurant)
+rest2=Restaurant("Felipe", " tortas")
+rest2.describe_restaruant()
+
+rest3=Restaurant("china","comida")
+rest3.describe_restaruant()
+
+rest4=Restaurant("el paseo", "comidaMexicana")
+rest4.describe_restaruant()
+
+ 
