5ba10d7e8487c2dfe2e49c2ff1d57e93e34438e5
Angel Santiago Jaime Zavala <AnhellO@users.noreply.github.com>
2018-11-15 02:40:09 +0000

    Update que-sera.py

--- Ago-Dic-2018/Prácticas/2do Parcial/que-sera.py
+++ Ago-Dic-2018/Prácticas/2do Parcial/que-sera.py
@@ -3,7 +3,7 @@
 
 import time, re, requests, os, errno, json, sqlite3
 
-directorio = 'archivitos-de-cache'
+directorio = 'archivitos-de-html'
 url = 'https://api.punkapi.com/v2/beers'
 
 ids = [1, 68, 28, 50, 200]
@@ -16,24 +16,45 @@ db = sqlite3.connect('basesita-de-datos.db')
 
 cursor = db.cursor()
 cursor.execute('''
-	CREATE TABLE chelas (
+	CREATE TABLE IF NOT EXISTS chelas (
 		id INTEGER PRIMARY KEY,
-		name TEXT
+		name TEXT,
+		description TEXT,
+		image VARCHAR(200)
 	)'''
 )
 
+queryto = cursor.execute('''
+	SELECT * FROM chelas
+	ORDER BY name
+''')
+
+for row in queryto:
+	nombreArchivo = '{}/{}.html'.format(directorio, row[1])
+	print('Escribiendo archivo {}...'.format(nombreArchivo))
+
+	if not os.path.exists(os.path.dirname(nombreArchivo)):
+		try:
+			os.makedirs(os.path.dirname(nombreArchivo))
+		except OSError as exc:
+			if exc.errno != errno.EEXIST:
+				raise
+
+	with open(nombreArchivo, 'w') as archivo:
+		waGuardar = '''
+		<h1>{}</h1>
+
+		<p>
+			{}
+			<br/>
+			<a href="{}">Imagen</a>
+		</p>
+
+		<em>Mostrando registro #{}</em>
+		'''.format(row[1], row[2], row[3], row[0])
+
+		archivo.write(waGuardar)
+		archivo.close()
+		print('Archivito cacheado :D!')
+
 db.close()
-# nombreArchivo = '{}/{}.json'.format(directorio, 'resultado')
-# print('Escribiendo archivo {}...'.format(nombreArchivo))
-#
-# if not os.path.exists(os.path.dirname(nombreArchivo)):
-# 	try:
-# 		os.makedirs(os.path.dirname(nombreArchivo))
-# 	except OSError as exc:
-# 		if exc.errno != errno.EEXIST:
-# 			raise
-#
-# with open(nombreArchivo, 'w') as archivo:
-# 	archivo.write(request.text)
-# 	archivo.close()
-# 	print('Archivito cacheado :D!')
