34c0eda7aa4e99a6219f22cacbc208774e2f214d
Angel Santiago Jaime Zavala <AnhellO@users.noreply.github.com>
2019-02-07 18:58:05 -0600

    Update clase-2.py

--- Ene-Jun-2019/Ejemplos/clase-2.py
+++ Ene-Jun-2019/Ejemplos/clase-2.py
@@ -31,4 +31,13 @@ operaciones.append(variable1 ** variable2)
 # """.format(variable1, variable2, *operaciones))
 
 for operacion in operaciones:
-    print(operacion)
+    pass#print(operacion)
+
+# Genera una lista con todos los números pares del 1 al 100
+# [i for i in range(0, 100, 2)] o [i for i in range(0, 100) if i % 2 == 0]
+
+# Genera una lista con todas las vocales existentes en un string
+# [i for i in 'Diseño y Arquitectura de Software' if i in 'aeiou']
+
+# Genera una lista con todos los caracteres del string, y convierte a mayúsculas aquellos caractéres que son vocales
+# [i.upper() if i in 'aeiou' else i for i in 'diseño y arquitectura de software']
