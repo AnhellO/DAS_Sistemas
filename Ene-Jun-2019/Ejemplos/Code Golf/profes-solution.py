887b30775ed17e771c88a88d96e657f0ef1da4b9
Angel Santiago Jaime Zavala <AnhellO@users.noreply.github.com>
2019-04-11 21:06:07 -0500

    Update profes-solution.py

--- Ene-Jun-2019/Ejemplos/Code Golf/profes-solution.py
+++ Ene-Jun-2019/Ejemplos/Code Golf/profes-solution.py
@@ -1,1 +1,1 @@
-print('\n'.join([' '.join(map(str,[*range(1,i+1)])) for i in [5, 10, 3, 18, 60, 99, 1000, 5000, 55555, 777777]]),end='')
+print(*[' '.join(map(str,[*range(1,i+1)])) for i in [5,10,3,18,60,99,1000,5000,55555,777777]],sep='\n',end='')
