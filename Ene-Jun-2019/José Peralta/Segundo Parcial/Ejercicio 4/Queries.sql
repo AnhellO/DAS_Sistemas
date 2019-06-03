9ea730be2d53b3b4820f371779c277606d1ba5b5
Luis <luis_al_or@hotmail.com>
2019-06-02 23:36:48 -0500

    Eliminando parte de Peralta

--- Ene-Jun-2019/José Peralta/Segundo Parcial/Ejercicio 4/Queries.sql
+++
@@ -1,6 +0,0 @@
-SELECT Correo FROM Users;
-
-SELECT COUNT(Genero) AS Mujeres FROM Users WHERE Genero='F';
-SELECT COUNT(Genero) AS Hombres FROM Users WHERE Genero='M';
-
-SELECT Nombre, Apellido, Correo, Direccion FROM Users;
