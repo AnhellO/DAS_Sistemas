3ed6676cf505d9f20f05d4c0239d7b836335d6b8
Angel Santiago Jaime Zavala <anhell.death999@gmail.com>
2020-03-05 18:51:42 -0600

    Agrega práctica 3

---
+++ Ene-Jun-2020/Practicas/1er Parcial/Practica 3/prototype.py
@@ -0,0 +1,17 @@
+class Oveja(object):
+	"""docstring for Oveja"""
+	def __init__(self, nombre, tipo):
+		self.nombre = nombre
+		self.tipo = tipo
+
+	def set_nombre(self, nombre):
+		self.nombre
+
+	def get_nombre(self):
+		return self.nombre
+
+	def set_tipo(self, tipo):
+		self.tipo
+
+	def get_tipo(self):
+		return self.tipo
