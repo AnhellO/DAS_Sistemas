4555cec1176d182a935b1f27702511b5714de9c4
EmilioBG1997 <emilio.barrera1997@gmail.com>
2020-03-11 15:15:51 -0600

    practica 3

--- Ene-Jun-2020/Practicas/1er Parcial/Practica 3/prototype.py
+++ Ene-Jun-2020/Practicas/1er Parcial/Practica 3/prototype.py
@@ -5,13 +5,15 @@ class Oveja(object):
 		self.tipo = tipo
 
 	def set_nombre(self, nombre):
-		self.nombre
+		self.nombre=nombre
 
 	def get_nombre(self):
 		return self.nombre
 
 	def set_tipo(self, tipo):
-		self.tipo
+		self.tipo=tipo
 
 	def get_tipo(self):
 		return self.tipo
+	def clone(self):
+		return Oveja(self.nombre,self.tipo)
