ca1a084387459e0f2b35c6ababfbde43eadd8602
ERORESRA <erick_escarcega@hotmail.com>
2020-09-09 23:42:16 -0500

    actualizacion de carpetas y cambios en practica-1

--- Ago-Dic-2020/escarcega-ramirez-erick-orlando/Practicas/Practica-1/practica-1.py
+++ Ago-Dic-2020/escarcega-ramirez-erick-orlando/Practicas/Practica-1/practica-1.py
@@ -1,17 +1,15 @@
-import numpy as np
-
 class Matrix:
     def __init__(self, matrix_string):
-       self.matrix = [[int(a) for a in line.split(' ')] for line in matrix_string.splitlines()]
+       self.matrix = [[int(a) for a in line.split()] for line in matrix_string.splitlines()]
 
     def row(self, row):
         return self.matrix[row-1]
 
     def column(self, col):
-        return [self.matrix[k][col-1] for k in range(len(self.matrix))]
+        #return [self.matrix[k][col-1] for k in enumerate(len(self.matrix))]
+        return [k[col-1] for k in self.matrix]
         
 end = Matrix("9 8 7\n5 3 2\n6 6 7")
 
-print(np.matrix(end.matrix))
 print(end.row(1))
 print(end.column(2))
