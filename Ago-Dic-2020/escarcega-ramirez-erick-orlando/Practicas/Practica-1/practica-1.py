ea66f74b5c763b98179db9c300a79106c7ec137c
ERORESRA <erick_escarcega@hotmail.com>
2020-09-10 14:57:33 -0500

    cambios en funciones utilizando el metodo copy()

--- Ago-Dic-2020/escarcega-ramirez-erick-orlando/Practicas/Practica-1/practica-1.py
+++ Ago-Dic-2020/escarcega-ramirez-erick-orlando/Practicas/Practica-1/practica-1.py
@@ -3,11 +3,11 @@ class Matrix:
        self.matrix = [[int(a) for a in line.split()] for line in matrix_string.splitlines()]
 
     def row(self, row):
-        return self.matrix[row-1]
+        return (self.matrix.copy())[row-1]
 
     def column(self, col):
         #return [self.matrix[k][col-1] for k in enumerate(len(self.matrix))]
-        return [k[col-1] for k in self.matrix]
+        return [k[col-1] for k in (self.matrix.copy())]
         
 end = Matrix("9 8 7\n5 3 2\n6 6 7")
 
