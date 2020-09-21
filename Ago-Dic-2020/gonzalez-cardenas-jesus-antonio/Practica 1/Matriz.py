0dc6dc9e2eff90cf93e99758f9601acd14b27108
J. AGC <47164685+JesusAGC@users.noreply.github.com>
2020-09-21 02:04:21 -0500

    Update Matriz.py

--- Ago-Dic-2020/gonzalez-cardenas-jesus-antonio/Practica 1/Matriz.py
+++ Ago-Dic-2020/gonzalez-cardenas-jesus-antonio/Practica 1/Matriz.py
@@ -1,14 +1,11 @@
 class Matrix:
     def __init__(self, matrix_string):
-        
-        self._matrix_string = [[int(row_item) for row_item in row.split()] for row in matrix_string.split("\n")]   
+       self._matrix_string = [[int(row_item) for row_item in row.split()] for row in matrix_string.split("\n")]   
 
     def row(self, index):
-        
         return self._matrix_string[index - 1]
 
     def column(self, index):
-        
         return [column_item[index - 1] for column_item in self._matrix_string]
         
 mtrx = Matrix("1 2 3\n4 5 6\n7 8 9")
