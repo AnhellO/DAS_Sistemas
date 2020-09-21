ea1618fc7adb3c9023e5f2b3b110d007f6dd46d1
David Sena <42477729+DavidSMtz@users.noreply.github.com>
2020-09-21 17:25:05 -0500

    DAS_Sistemas develop push: Practica 2

---
+++ Ago-Dic-2020/sena-martinez-angel-david/Practica2/Practica2.py
@@ -0,0 +1,37 @@
+class Persona():
+    def __init__(self, edad, peso, altura):
+        self.edad = edad
+        self.peso = peso
+        self.altura = altura
+
+    def _str_(self):
+        return f"Edad: {self.edad} años\nPeso:{self.peso}\nAltura:{self.altura}cm"
+    
+    def pesoideal(self):
+        self.peso_ideal = 0.75 * (self.altura * 100) - 62.5
+        return f"El peso ideal de esta persona es de: {self.peso_ideal}"
+    
+    def imc(self):
+        self.imc = self.peso / (self.altura * self.altura)
+        return f"El Indice de Masa Corporal de esta persona es de: {self.imc}"
+
+class Estudiante(Persona):
+    def __init__(self, edad, peso, altura, nombre):
+        Persona.__init__(self,edad,peso,altura)
+        self.nombre = nombre
+
+    def descripcion(self):
+        return f"{self.nombre} tiene {self.edad} años, su peso es de {self.peso}y su altura es de {self.altura} cm."
+   
+    def hijos(self,sons):
+        return f"{self.nombre} tiene {sons} hijos"
+    
+    
+
+if __name__ == "__main__":
+    student = Estudiante(25, 90, 1.80, "David")
+    print(student)
+    print(student.descripcion())
+    print(student.pesoideal())
+    print(student.imc())
+    print(student.hijos(4))
