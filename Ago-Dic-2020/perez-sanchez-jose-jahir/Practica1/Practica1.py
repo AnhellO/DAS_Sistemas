2b1daa8da2312767883c4d75db7a92fc154a71c5
jahirrlly <jahirps98@outlook.com>
2020-09-22 10:41:21 -0500

    Cambios

---
+++ Ago-Dic-2020/perez-sanchez-jose-jahir/Practica1/Practica1.py
@@ -0,0 +1,22 @@
+class Matriz(str):
+    def __init__(self, string):
+        #Separaramos nuestro string por los saltos de linea y cada salto de linea lo separamos por espacios vacios para asi obtener una lista
+        self.matriz = [[x for x in h.split()] for h in string.splitlines()]
+
+    def row(self, index):  
+        #Regresa la parte de la lista que se refiera a ese numero de fila 
+        num = self.matriz[index-1]
+        return f"Los valores en la fila {index} son: {num}"
+
+    def column(self,index):
+        #Regresas la columna gracias al indice
+        num = [x[index-1] for x in self.matriz]
+        return f"Los valores en la columna {index} son: {num}"
+
+
+if __name__ == "__main__":
+    x = Matriz("9 8 7\n5 3 2\n6 6 7")
+    print(x.matriz)
+    print(x.row(1))
+    print(x.column(1))
+
