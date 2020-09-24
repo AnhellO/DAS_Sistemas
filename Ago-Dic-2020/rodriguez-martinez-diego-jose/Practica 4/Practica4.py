d5a97aeef1e49811ff70761a78673aa356306ca1
D. Rodriguez Martinez <d.rdz.mtz314@gmail.com>
2020-09-24 14:22:50 -0500

    Practica 4

---
+++ Ago-Dic-2020/rodriguez-martinez-diego-jose/Practica 4/Practica4.py
@@ -0,0 +1,61 @@
+from abc import ABC, abstractmethod
+
+class SchoolMember(ABC):
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name', '')
+        self.age = kwargs.get('age', -1)
+        self.id_num = kwargs.get('id', 'XXXXXX')
+    
+    def __str__(self):
+        return f"Soy el {type(self).__name__} {self.name}!, tengo {self.age} años y mi ID = {self.id_num}"
+
+    @abstractmethod
+    def funcionDe(self):
+        """ Returnea la funcion del puesto"""
+
+        return f"Funcion de {type(self).__name__}: \n"
+
+##################
+class Teacher(SchoolMember):
+
+    def funcionDe(self):
+        super().funcionDe()
+        return "Persona que ayuda a los estudiantes a adquirir conocimientos"
+
+##################
+class Janitor(SchoolMember):
+
+    def funcionDe(self):
+        super().funcionDe()
+        return "Persona empleada como cuidador del edificio de la escuela; limpia y mantiene los edificios"
+
+###################
+class Student(SchoolMember):
+
+    def funcionDe(self):
+        super().funcionDe()
+        return "Persona inscrita en una escuela con el objetivo de adquirir conocimientos"
+
+###################
+class SchoolMemberFactory:
+    @classmethod
+    def make(cls, kind, **kwargs):
+        clases = [cls.__name__ for cls in SchoolMember.__subclasses__()]
+
+        if kind.capitalize() not in clases:
+            raise ValueError("Error! tipo invalido!")
+
+        return eval(kind.capitalize())(**kwargs)
+
+
+#######
+#######
+def main():
+    kind = input("Qué quieres crear?\n")
+    _name = input("Qué nombre tiene?\n")
+    obj = SchoolMemberFactory.make(kind, name=_name, age=22, id='070KGP')
+    print(obj)
+    print(obj.funcionDe())
+    
+if __name__ == "__main__":
+    main()
