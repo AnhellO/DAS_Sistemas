c6415e8b2585b33aa05ed5cf9e58773ee3b2b212
Danieldm16 <salazar_daniel@hotmail.es>
2020-10-01 18:27:59 -0500

    DAS_Sistemas Develop Push:Cambios realizados a Practica 2

--- Ago-Dic-2020/salazar-coronado-juan-daniel/practica2/practica2.py
+++ Ago-Dic-2020/salazar-coronado-juan-daniel/practica2/practica2.py
@@ -6,52 +6,57 @@ class Estudiante():
         self.carrera = carrera
 
     def presentacion(self):
-        return "Soy " + self.nombre + ", tengo " + self.edad + " años y estoy en " + self.semestre + " semestre."
+        return f"Soy {self.nombre}, tengo {self.edad} años y estoy en {self.semestre} semestre."
 
-    def getCarrera(self):
+    def get_carrera(self):
         carreras = {
-        'ISC': "Ing. En sistemas computacionales",
-        'IIS': "Ing. Industrial y de sistemas",
-        'IEC': "Ing. En electronica y comunicaciones",
-        'ITC': "Ingeniero en tecnología y comunicaciones",
-        'LSA': "Lic. En sistemas computacionales y administrativos",
-        'IA': "Ingeniero automotriz"
+            'ISC': "Ing. En sistemas computacionales",
+            'IIS': "Ing. Industrial y de sistemas",
+            'IEC': "Ing. En electronica y comunicaciones",
+            'ITC': "Ingeniero en tecnología y comunicaciones",
+            'LSA': "Lic. En sistemas computacionales y administrativos",
+            'IA': "Ingeniero automotriz"
         }
         return "Estudio la carrera de " + carreras.get(self.carrera)
     
-    def MotivacionPromedio(self, promedio):
+    def motivacion_promedio(self, promedio):
         if promedio >= 70 and promedio <= 79:
             return "necesito esforzarme más"
-        elif promedio > 79 and promedio <= 89:
+        
+        if promedio > 79 and promedio <= 89:
             return "lo estoy haciendo bien, pero puedo hacerlo mejor"
-        elif promedio >= 90 and promedio <= 100:
+        
+        if promedio >= 90 and promedio <= 100:
             return "voy muy bien, debo seguir así"
-        else:
-            return "-> promedio fuera de los parametros normales <-"
+        
+        return "-> promedio fuera de los parametros normales <-"
 
 class ServicioSocialYPracticas(Estudiante):
     def __init__(self, nombre, edad, semestre, carrera, servicio, practicas):
-        Estudiante.__init__(self, nombre, edad, semestre, carrera)
+        super().__init__(nombre, edad, semestre, carrera)
         self.servicio = servicio
         self.practicas = practicas
         
-    def Estado(self):
-        if self.servicio == True and self.practicas == True:
+    def estado(self):
+        if self.servicio and self.practicas:
             return "Ya realice el servicio social y las practicas profesionales"
-        elif self.servicio == False and self.practicas == False:
+        
+        if not self.servicio and not self.practicas:
             return "No he realizado el servicio social ni las practicas profesionales"
-        elif self.servicio == True and self.practicas == False:
+        
+        if self.servicio == True and self.practicas == False:
             return "Ya realice el servicio social pero no he realizado las practicas profesionales"
-        elif self.servicio == False and self.practicas == True:
+        
+        if self.servicio == False and self.practicas == True:
             return "No he realizado el servicio social pero ya realice las practicas profesionales"
 
-    def ProgramasServicio(self):
+    def programas_servicio(self):
         programas = ['Identidad institucional', 'Asistente de investigación', 'Pagina Web Sistemas']
         if self.servicio == False:
             print("Puedes realizar el servicio social en los siguientes programas: ")
             return programas
 
-    def EmpresasPracticas(self):
+    def empresas_practicas(self):
         empresas = ['Banamex', 'Central de talento universitario SC', 'Tecno Profesionales Rubik']
         if self.practicas == False:
             print("Puedes realizar las practicas profesionales en las siguientes empresas: ")
@@ -60,12 +65,12 @@ class ServicioSocialYPracticas(Estudiante):
 estudiante1 = Estudiante("Daniel", "21", "Septimo", "ISC")
 
 print(estudiante1.presentacion())
-print(estudiante1.getCarrera())
-print("En base a mi promedio " + estudiante1.MotivacionPromedio(88.34))
+print(estudiante1.get_carrera())
+print("En base a mi promedio " + estudiante1.motivacion_promedio(88.34))
 
 estudiante1ssyp = ServicioSocialYPracticas("Daniel", "21", "Septimo", "ISC", False, False)
 
-print(estudiante1ssyp.Estado())
-print(estudiante1ssyp.ProgramasServicio())
-print(estudiante1ssyp.EmpresasPracticas())
+print(estudiante1ssyp.estado())
+print(estudiante1ssyp.programas_servicio())
+print(estudiante1ssyp.empresas_practicas())
 
