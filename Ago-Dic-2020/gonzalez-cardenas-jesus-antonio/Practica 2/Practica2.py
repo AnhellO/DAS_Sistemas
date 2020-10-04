2932599e9c6cbe93d45398fb9c3b3f5af5b8a615
Jesús González <jesus_glezz229@hotmail.com>
2020-10-03 23:59:05 -0500

    Commit modificaciones de practica 2

--- Ago-Dic-2020/gonzalez-cardenas-jesus-antonio/Practica 2/Practica2.py
+++ Ago-Dic-2020/gonzalez-cardenas-jesus-antonio/Practica 2/Practica2.py
@@ -1,75 +1,75 @@
 class Vehiculo(object):
-    def __init__ (self, name, dateRelease, passengers, numOfWheels, terrain, typeVehicle):
+    def __init__(self, name, date_of_release, passengers, number_of_wheels, terrain, type_of_vehicle):
         self.name = name
-        self.dateRelease = dateRelease
+        self.date_of_release = date_of_release
         self.passengers = passengers
-        self.numOfWheels = numOfWheels
+        self.number_of_wheels = number_of_wheels
         self.terrain = terrain
-        self.typeVehicle = typeVehicle
+        self.type_of_vehicle = type_of_vehicle
 
     def specs(self):
-        print(self.name+' salió en la fecha '+ self.dateRelease +', tiene el número de llantas: ' +str(self.numOfWheels) +'. Y es para el terreno tipo: ' +self.terrain)
+        print(f"{self.name} salió en la fecha: {self.date_of_release}, tiene el número de llantas: {self.number_of_wheels} y es para el terreno tipo: {self.terrain}")
         print('Numero de pasajeros = ' + str(self.passengers))
 
     def movev(self, km):
-        print(self.name +' se ha movido ' + str(km)+' unidades')
+        print(f"{self.name} se ha movido {km} unidades")
 
     def stop(self):
         print(self.name +' se ha detenido')
 
 class Camion(Vehiculo):
-    NumActualDePasajeros = 0
-    Cuenta = 0.000
+    numero_actual_de_pasajeros = 0
+    cuenta = 0.000
     cobro = 0.000
-    def __init__ (self, name, dateRelease, passengers, numOfWheels, terrain, typeVehicle,):
-        Vehiculo.__init__(self, name, dateRelease, passengers, numOfWheels, terrain, typeVehicle)
+    def __init__(self, name, date_of_release, passengers, number_of_wheels, terrain, type_of_vehicle,):
+        Vehiculo.__init__(self, name, date_of_release, passengers, number_of_wheels, terrain, type_of_vehicle)
 
-    def SubePasajero (self, numSub):
-        numSub = int(numSub)
-        caja  = float(numSub) * self.cobro
-        self.Cuenta = self.Cuenta + caja
-        self.NumActualDePasajeros = self.NumActualDePasajeros + numSub
-        print('Se han subido ' +str(numSub)+ ', se ha ingresado:'+str(caja))
+    def SubePasajero (self, numero_de_pasajeros_que_subieron):
+        numero_de_pasajeros_que_subieron = int(numero_de_pasajeros_que_subieron)
+        caja  = float(numero_de_pasajeros_que_subieron) * self.cobro
+        self.cuenta = self.cuenta + caja
+        self.numero_actual_de_pasajeros = self.numero_actual_de_pasajeros + numero_de_pasajeros_que_subieron
+        print(f"Se han subido {numero_de_pasajeros_que_subieron}, se ha ingresado: {caja}")
 
-    def BajaPasajero(self, numBaj):
-        self.NumActualDePasajeros = self.NumActualDePasajeros - numBaj
-        print('Se han bajado ' +str(numBaj))
+    def BajaPasajero(self, numero_de_pasajeros_que_bajaron):
+        self.numero_actual_de_pasajeros = self.numero_actual_de_pasajeros - numero_de_pasajeros_que_bajaron
+        print(f"Se han bajado {numero_de_pasajeros_que_bajaron}")
 
     def CamionStatus(self):
-        print('Hay '+str(self.NumActualDePasajeros)+' actualmente en el camión y hay '+str(self.Cuenta)+' en la cuenta')
+        print(f"Hay {self.numero_actual_de_pasajeros} actualmente en el camión y hay {self.cuenta} en la cuenta")
 
     def SetCuota(self,cta):
         cta = float(cta)
         self.cobro = cta
 
 class Jet(Vehiculo):
-    def __init__ (self, name, dateRelease, passengers, numOfWheels, terrain, typeVehicle, MGAmmo, MissileAmmo):
-        self.MGAmmo = MGAmmo
-        self.MissileAmmo = MissileAmmo
-        Vehiculo.__init__(self, name, dateRelease, passengers, numOfWheels, terrain, typeVehicle)
+    def __init__(self, name, date_of_release, passengers, number_of_wheels, terrain, type_of_vehicle, mg_ammo, missile_ammo):
+        self.mg_ammo = mg_ammo
+        self.missile_ammo = missile_ammo
+        Vehiculo.__init__(self, name, date_of_release, passengers, number_of_wheels, terrain, type_of_vehicle)
 
-    def ShootMG(self,Shoots):
-        if Shoots > self.MGAmmo:
-            data1 = Shoots - self.MGAmmo
-            print('No se pueden hacer la cantidad de disparos solicitados, faltan '+str(data1)+' unidades de munición')
+    def ShootMG(self,shoots):
+        if shoots > self.mg_ammo:
+            data1 = shoots - self.mg_ammo
+            print(f"No se pueden hacer la cantidad de disparos solicitados, faltan {data1} unidades de munición")
         else:
-            self.MGAmmo = self.MGAmmo - Shoots
-            print('Se ha disparado satisfactoriamente, se usaron '+str(Shoots)+' unidades de munición')
+            self.mg_ammo = self.mg_ammo - shoots
+            print(f"Se ha disparado satisfactoriamente, se usaron {shoots} unidades de munición")
 
-    def ShootMissiles(self,Shoots):
-        if Shoots > self.MissileAmmo:
-            data1 = Shoots - self.MissileAmmo
-            print('No se pueden hacer la cantidad de disparos solicitados, faltan '+str(data1)+' misiles')
+    def ShootMissiles(self,shoots):
+        if shoots > self.missile_ammo:
+            data1 = shoots - self.missile_ammo
+            print(f"No se pueden hacer la cantidad de disparos solicitados, faltan {data1} misiles")
         else:
-            self.MissileAmmo = self.MissileAmmo - Shoots
-            print('Se ha disparado satisfactoriamente, se usaron '+str(Shoots)+' misiles')
+            self.missile_ammo = self.missile_ammo - shoots
+            print(f"Se ha disparado satisfactoriamente, se usaron {shoots} misiles")
 
     def JetStatus(self):
-        print('Munición de MG = ' +str(self.MGAmmo))
-        print('Munición de Misiles = ' +str(self.MissileAmmo))
+        print(f"Munición de MG = {self.mg_ammo}")
+        print(f"Munición de Misiles = {self.missile_ammo}")
 
     def SendMessageToBase(self, mess):
-        print('Tu mensaje "'+mess+ '" ha sido recibido por nuestra base')   
+        print(f"Tu mensaje {mess} ha sido recibido por nuestra base")   
         
 
 camion_uno = Camion('Mercedes-Benz O371', 'Decada de 1980', 28, 4, 'Urbano', 'Transporte publico')
