b6193455812afc0961ebade0e980eff2894b205a
Rephyroth <57422543+Rephyroth@users.noreply.github.com>
2020-10-04 18:32:30 -0500

    Cambios clases y diagrama

--- Ago-Dic-2020/flores-fernandez-fernando/Practicas/Practica-2/Practica-2.py
+++ Ago-Dic-2020/flores-fernandez-fernando/Practicas/Practica-2/Practica-2.py
@@ -6,20 +6,20 @@ class Dunedain:
         self.vivo = True
         self.sp = sp
     
-    def Saludar(self):
+    def saludar(self):
         print (f"Mae govannen mi nombre es: {self.nombre}")
     
-    def Comer(self,comida):
-        subhp=self.hp + comida
+    def comer(self,comida):
+        subhp = self.hp + comida
         print(f"Al comer tus HealtPoints subieron de: {str(self.hp)} A: {str(subhp)}")
-        self.hp=subhp 
+        self.hp = subhp 
     
-    def Cambiar_Ocupacion(self,ocupacion):
-        ocupacionco= self.ocupacion
+    def cambiar_ocupacion(self,ocupacion):
+        ocupacionco = self.ocupacion
         self.ocupacion = ocupacion 
         print(f"Tu Ocupacion cambió de: {ocupacionco} A: {self.ocupacion}")
     
-    def Status(self):
+    def status(self):
         print(f"Nombre: {self.nombre}\nOcupacion: {self.ocupacion}\nHealt Points: {str(self.hp)}\nStrike Points: {str(self.sp)}\nVivo: {str(self.vivo)}")
     
               
@@ -29,13 +29,13 @@ class Ranger(Dunedain):
         self.nombre = nombre
         self.ocupacion = ocupacion
         self.hp = hp
-        self.vivo= True 
-        self.sp=sp
+        self.vivo = True 
+        self.sp = sp
     
-    def Atacar(self):
+    def atacar(self):
         print(f"{self.nombre} Dio una estocada que infligio: {str(self.sp)} puntos de daño")
     
-    def Recibir_Daño(self,sp):
+    def recibir_daño(self,sp):
         if self.hp <= sp:
             print(f"{self.nombre} Recibió: {str(sp)} Puntos de daño\n{self.nombre} A muerto")
             self.vivo=False
@@ -49,9 +49,9 @@ d1 = Dunedain("Nan","Herrero",50,20)
 
 r1 = Ranger("Fer","Ranger",50,30)
 
-d1.Saludar()
-r1.Atacar()
-r1.Recibir_Daño(20)
-r1.Comer(20)
-r1.Recibir_Daño(80)
-r1.Status()
+d1.saludar()
+r1.atacar()
+r1.recibir_daño(20)
+r1.comer(20)
+r1.recibir_daño(80)
+r1.status()
