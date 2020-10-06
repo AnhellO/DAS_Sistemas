17d578a99897b2ccceffcb71a8f9241e10210d71
Arnoldo <juan.arnoldo.munoz@hotmail.com>
2020-10-06 16:46:50 -0500

    DAS_Sistemas Develop Push: cambios de la practica-1

--- Ago-Dic-2020/chavez-muñoz-juan-arnoldo/Practica-1/Practica-1.py
+++ Ago-Dic-2020/chavez-muñoz-juan-arnoldo/Practica-1/Practica-1.py
@@ -1,15 +1,13 @@
 class Matrix:
-    matrix_string =[]
-    
     def __init__(self, matrix_string):#constructor de la clase
-        self.matrix_string = [[int(num) for num in lista.split()] for lista in matrix_string.split("\n")]
+        self.matrix_string = [[int(num) for num in lista.split()] for lista in matrix_string.splitlines()]
 
     def row(self, index):
-            return print(self.matrix_string[index-1])
+        return self.matrix_string[index-1]
         
     def column(self, index):
-        return print([self.matrix_string[i][index-1] for i in range( len(self.matrix_string))])
+        return [self.matrix_string[i][index-1] for i in range(len(self.matrix_string))]
         
 matriz = Matrix("9 8 7\n 5 3 2\n6 6 7") #Creo la instancia de la clases
-matriz.row(1)
-matriz.column(2)
+print(matriz.row(1))
+print(matriz.column(2))
