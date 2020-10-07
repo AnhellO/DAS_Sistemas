0d81a05acaa84eb32ca566507df0ab05bb73ecf0
“Jesús <jesus_glezz229@hotmail.com>
2020-10-06 21:49:53 -0500

    Commit de practica 2 - 3.1 Añado archivos que faltaron en el commit anterior

--- Ago-Dic-2020/gonzalez-cardenas-jesus-antonio/Practica 2/Practica2.py
+++ Ago-Dic-2020/gonzalez-cardenas-jesus-antonio/Practica 2/Practica2.py
@@ -11,10 +11,10 @@ class Vehiculo(object):
         print(f"{self.name} salió en la fecha: {self.date_of_release}, tiene el número de llantas: {self.number_of_wheels} y es para el terreno tipo: {self.terrain}")
         print('Numero de pasajeros = ' + str(self.passengers))
 
-    def movev(self, km):
+    def move_vehicle(self, km):
         print(f"{self.name} se ha movido {km} unidades")
 
-    def stop(self):
+    def stop_vehicle(self):
         print(self.name +' se ha detenido')
 
 class Camion(Vehiculo):
@@ -24,21 +24,21 @@ class Camion(Vehiculo):
     def __init__(self, name, date_of_release, passengers, number_of_wheels, terrain, type_of_vehicle,):
         super().__init__(name, date_of_release, passengers, number_of_wheels, terrain, type_of_vehicle)
 
-    def SubePasajero (self, numero_de_pasajeros_que_subieron):
+    def sube_pasajero (self, numero_de_pasajeros_que_subieron):
         numero_de_pasajeros_que_subieron = int(numero_de_pasajeros_que_subieron)
         caja  = float(numero_de_pasajeros_que_subieron) * self.cobro
         self.cuenta = self.cuenta + caja
         self.numero_actual_de_pasajeros = self.numero_actual_de_pasajeros + numero_de_pasajeros_que_subieron
         print(f"Se han subido {numero_de_pasajeros_que_subieron}, se ha ingresado: {caja}")
 
-    def BajaPasajero(self, numero_de_pasajeros_que_bajaron):
+    def baja_pasajero(self, numero_de_pasajeros_que_bajaron):
         self.numero_actual_de_pasajeros = self.numero_actual_de_pasajeros - numero_de_pasajeros_que_bajaron
         print(f"Se han bajado {numero_de_pasajeros_que_bajaron}")
 
-    def CamionStatus(self):
+    def camion_status(self):
         print(f"Hay {self.numero_actual_de_pasajeros} actualmente en el camión y hay {self.cuenta} en la cuenta")
 
-    def SetCuota(self,cta):
+    def set_cuota(self,cta):
         cta = float(cta)
         self.cobro = cta
 
@@ -48,7 +48,7 @@ class Jet(Vehiculo):
         self.missile_ammo = missile_ammo
         super().__init__(name, date_of_release, passengers, number_of_wheels, terrain, type_of_vehicle)
 
-    def ShootMG(self,shoots):
+    def shoot_mg(self,shoots):
         if shoots > self.mg_ammo:
             data1 = shoots - self.mg_ammo
             print(f"No se pueden hacer la cantidad de disparos solicitados, faltan {data1} unidades de munición")
@@ -56,7 +56,7 @@ class Jet(Vehiculo):
             self.mg_ammo = self.mg_ammo - shoots
             print(f"Se ha disparado satisfactoriamente, se usaron {shoots} unidades de munición")
 
-    def ShootMissiles(self,shoots):
+    def shoot_missiles(self,shoots):
         if shoots > self.missile_ammo:
             data1 = shoots - self.missile_ammo
             print(f"No se pueden hacer la cantidad de disparos solicitados, faltan {data1} misiles")
@@ -64,32 +64,32 @@ class Jet(Vehiculo):
             self.missile_ammo = self.missile_ammo - shoots
             print(f"Se ha disparado satisfactoriamente, se usaron {shoots} misiles")
 
-    def JetStatus(self):
+    def jet_status(self):
         print(f"Munición de MG = {self.mg_ammo}")
         print(f"Munición de Misiles = {self.missile_ammo}")
 
-    def SendMessageToBase(self, mess):
-        print(f"Tu mensaje {mess} ha sido recibido por nuestra base")   
+    def send_message_to_base(self, mess):
+        print(f"Tu mensaje :\n{mess}\nha sido recibido por nuestra base")   
         
 
 camion_uno = Camion('Mercedes-Benz O371', 'Decada de 1980', 28, 4, 'Urbano', 'Transporte publico')
 jet_uno = Jet('F-22 Raptor', 'Año 2003', 1, 3, 'Aereo', 'Combate', 5000, 15)
 
 camion_uno.specs()
-camion_uno.movev(12)
-camion_uno.stop()
-camion_uno.SetCuota(11.5)
-camion_uno.SubePasajero(5)
-camion_uno.BajaPasajero(2)
-camion_uno.CamionStatus()
+camion_uno.move_vehicle(12)
+camion_uno.stop_vehicle()
+camion_uno.set_cuota(11.5)
+camion_uno.sube_pasajero(5)
+camion_uno.baja_pasajero(2)
+camion_uno.camion_status()
 print('\n')
 
 jet_uno.specs()
-jet_uno.movev(1000)
-jet_uno.stop()
-jet_uno.ShootMG(1000)
-jet_uno.ShootMissiles(7)
-jet_uno.JetStatus()
-jet_uno.SendMessageToBase('Acabo de cometer un crimen de guerra')
+jet_uno.move_vehicle(1000)
+jet_uno.stop_vehicle()
+jet_uno.shoot_mg(1000)
+jet_uno.shoot_missiles(7)
+jet_uno.jet_status()
+jet_uno.send_message_to_base('Acabo de cometer un crimen de guerra')
 
 
