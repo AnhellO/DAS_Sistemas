7965dfff34444239ee95c73f69dbb35f716b6c5d
David Sena <42477729+DavidSMtz@users.noreply.github.com>
2020-10-06 23:41:06 -0500

    DAS_Sistemas develop push:Cambios Practica 3

--- Ago-Dic-2020/sena-martinez-angel-david/Practica3/Flyweight_Test.py
+++ Ago-Dic-2020/sena-martinez-angel-david/Practica3/Flyweight_Test.py
@@ -1,11 +1,10 @@
 import unittest
 from Flyweight import *
 
-
 class TargetTest(unittest.TestCase):
     def test_car(self):
         car = CarFamilies("audi",12345)
-        self.assertEqual(CarFamilies(car)), "Objetivo: comportamiento del objetivo predeterminado")
+        self.assertEqual(CarFamilies(car)),("Objetivo: comportamiento del objetivo predeterminado")
 
 if __name__ == "__main__":
     unittest.main() 
