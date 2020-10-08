ea46167da86df1bdf05d596951a086eb3feca81d
jahirrlly <jahirps98@outlook.com>
2020-10-08 10:46:01 -0500

    Los cambios solicitados

--- Ago-Dic-2020/perez-sanchez-jose-jahir/Practica4/Practica4.py
+++ Ago-Dic-2020/perez-sanchez-jose-jahir/Practica4/Practica4.py
@@ -1,38 +1,30 @@
 import abc
 from abc import abstractmethod, ABC  
-class SchoolMember():
+
+class SchoolMember(ABC):
     def __init__(self, **args):
         self.name = args.get('name', '')
         self.age = args.get('age', -1)
         self.id_num = args.get('id_num', 'XXXXXX')
     
     def __str__(self):
-        return f"Soy {self.name}!, tengo {self.age} años y mi ID = {self.id_num}"
+        return f"Soy {self.name}!, soy {self.obtener_tipo()}, tengo {self.age} años y mi ID = {self.id_num}"
     
     @abstractmethod
-    def obtener_id(self):
+    def obtener_tipo(self):
         pass
 
-class Teacher(SchoolMember):
-    def __str__(self):
-        return super().__str__().replace("Soy", "Soy el ticher")
-    
-    def obtener_id(self):
-        return "XXXXXX"
+class Teacher(SchoolMember):  
+    def obtener_tipo(self):
+        return "Maestro"
 
 class Student(SchoolMember):
-    def __str__(self):
-        return super().__str__().replace("Soy", "Soy el alumno")
-
-    def obtener_id(self):
-        return "XXXXXX"
+    def obtener_tipo(self):
+        return "Estudiante"
         
-class Director(SchoolMember):
-    def __str__(self):
-        return super().__str__().replace("Soy","Soy el director")
-    
-    def obtener_id(self):
-        return "XXXXXX"
+class Director(SchoolMember):  
+    def obtener_tipo(self):
+        return "Director"
         
 class SchoolMemberFactory:
     @classmethod
@@ -41,7 +33,7 @@ class SchoolMemberFactory:
             return eval(kind.capitalize())(**args)
         except Exception:
             return f"Error! tipo '{kind}' invalido!"
-        except args.get(len(id)) !=6:
+        except args.get(len(id)) != 6:
             return "Error! tipo ID invalido!"
         
 def main():
