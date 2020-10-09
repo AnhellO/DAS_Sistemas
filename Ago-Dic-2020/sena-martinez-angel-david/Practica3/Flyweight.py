8e01d72576741abf30f176a0577f121069612322
David Sena <42477729+DavidSMtz@users.noreply.github.com>
2020-10-09 02:58:29 -0500

    DAS_Sistemas develop push: Omita este archivo

--- Ago-Dic-2020/sena-martinez-angel-david/Practica3/Flyweight.py
+++ Ago-Dic-2020/sena-martinez-angel-david/Practica3/Flyweight.py
@@ -2,15 +2,23 @@ from abc import ABC, abstractclassmethod
 import requests
 
 class ComplexCars(object): 
-    #Separar clases para carros complejos
+  
+    """Separar clases para carros complejos"""
+  
     def __init__(self): 
+  
         pass
-    
+  
     def cars(self, car_name): 
+  
         return "Patron Complejo[% s]" % (car_name) 
 
+  
+  
 class CarFamilies(object): 
-    #creamos un diccionario para almacenar el tipo de carro
+  
+    """creamos un diccionario para almacenar el tipo de carro"""
+  
     car_family = {} 
   
     def __new__(cls, name, car_family_id): 
@@ -22,13 +30,19 @@ class CarFamilies(object):
         return id
   
     def set_car_info(self, car_info): 
+  
         """configurar informacion del carro"""
+  
         cg = ComplexCars() 
         self.car_info = cg.cars(car_info) 
   
     def get_car_info(self): 
-    #retorna la informacion del carro
+  
+        """retorna la informacion del carro"""
+  
         return (self.car_info) 
+  
+  
   
 if __name__ == '__main__': 
     car_data = (('a', 1, 'Audi'), ('a', 2, 'Ferrari'), ('b', 1, 'Audi')) 
@@ -37,7 +51,9 @@ if __name__ == '__main__':
         obj = CarFamilies(i[0], i[1]) 
         obj.set_car_info(i[2]) 
         car_family_objects.append(obj) 
-  #identificaciones similares dicen que son los mismos objetos 
+  
+    """identificaciones similares dicen que son los mismos objetos """
+  
     for i in car_family_objects: 
         print("id = " + str(id(i))) 
         print(i.get_car_info()) 
