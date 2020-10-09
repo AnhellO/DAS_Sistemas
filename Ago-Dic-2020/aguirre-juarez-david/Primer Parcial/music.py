5ec50566124b080c6230ec4a7c0d38e291e0faec
David Aguirre <daguirrez2000@gmail.com>
2020-10-08 22:39:20 -0500

    Ejercicio 2

--- Ago-Dic-2020/aguirre-juarez-david/Primer Parcial/music.py
+++ Ago-Dic-2020/aguirre-juarez-david/Primer Parcial/music.py
@@ -1,5 +1,6 @@
 import enum
 import time
+from abc import ABC, abstractmethod
 
 class Gender(enum.Enum):
 	none = 0
@@ -32,17 +33,34 @@ class Song:
 		)
 
 	def print_animated_name(self, letters_on_screen = 5):
-		buffer = f"${' '*letters_on_screen}${self.name}${' '*letters_on_screen}"
+		buffer = f"{' '*letters_on_screen}{self.name}{' '*letters_on_screen}"
 
 		for k in range(len(buffer)):
 			print(buffer[k:k + letters_on_screen], end = '\r')
 			time.sleep(0.2)
 
-class MusicApp:
+class MusicAppComponent(ABC):
+	@abstractmethod
+	def play_song(self, name):
+		pass
+
+class MusicApp(MusicAppComponent):
 	def __init__(self, premium = False):
 		self.__songs = []
 		self.__is_premium = premium
 		self.__dark_mode = False
+
+	def play_song(self, name):
+		songs = [s for s in self.__songs if s.name == name]
+
+		if len(songs) == 0:
+			print('Canción no encontrada...')
+			return None
+
+		# "start song" and return it
+		print(f"Iniciando canción {name}.")
+
+		return songs[0]
 
 	def add_song(self, song):
 		if not self.__is_premium and len(self.__songs) >= 5:
