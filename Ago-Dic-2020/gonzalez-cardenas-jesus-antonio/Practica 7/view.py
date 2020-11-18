ce98f8dc03edc689d219fe133da65c5931629754
Jesús González <jesus_glezz229@hotmail.com>
2020-11-18 13:36:05 -0600

    commit 1 - Practica 7 Inicializacion

---
+++ Ago-Dic-2020/gonzalez-cardenas-jesus-antonio/Practica 7/view.py
@@ -0,0 +1,30 @@
+import pymongo
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def Index():
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["persons"]
+    some_person_json = db.persons.find_one()
+    some_person = json.loads(some_person_json)
+    some_person = format_data(some_person)
+    return render_template('index.html', person = some_person)
+
+def format_data(data_person:dict):
+    person_info = {
+    'user_name' : data_person.get('person').get('online_info').get('username'),
+    'e_mail': data_person.get('person').get('online_info').get('email'),
+    'name':data_person.get('person').get('personal').get('name'),
+    'last_name':data_person.get('person').get('personal').get('last_name'),
+    'age':data_person.get('person').get('personal').get('age'),
+    'number':data_person.get('person').get('personal').get('cellphone'),
+    'country':data_person.get('person').get('personal').get('country')
+    }
+    return person_info
+
+if __name__ == "__main__":
+    app.run(debug = True    
+    )
