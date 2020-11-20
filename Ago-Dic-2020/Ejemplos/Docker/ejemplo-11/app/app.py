8e1ae7fb78f11d47f0ae3fd7d9c2022db83829bf
Angel Santiago Jaime Zavala <asjz2892@gmail.com>
2020-11-19 19:14:55 -0600

    Completa ejemplo 11

--- Ago-Dic-2020/Ejemplos/Docker/ejemplo-11/app/app.py
+++ Ago-Dic-2020/Ejemplos/Docker/ejemplo-11/app/app.py
@@ -1,11 +1,22 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+from models import MyCats
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return 'Index Pageee'
 
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
     name = request.form['name'] if request.method == 'POST' and 'name' in request.form else 'Anonymous'
     return render_template('hello.html', name=name)
+
+@app.route('/my-cats', methods=['GET', 'POST'])
+def questions():
+    if request.method == 'POST':
+        cat = MyCats(nombre=request.form['nombre'], imagen=request.form['imagen'])
+        cat.save()
+        return {'ID': cat.id, 'nombre': cat.nombre, 'imagen': cat.imagen}
+
+    return jsonify([{'ID': cat.id, 'nombre': cat.nombre, 'imagen': cat.imagen} for cat in MyCats.select()])
