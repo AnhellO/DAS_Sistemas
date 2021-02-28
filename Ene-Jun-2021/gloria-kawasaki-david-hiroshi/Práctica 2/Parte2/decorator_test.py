c54e816fe2ea50b6651b78ebbea6f1e8c635a65b
Hiroshi <sephirothMH@outlook.com>
2021-02-28 14:08:56 -0600

    Práctica 2

---
+++ Ene-Jun-2021/gloria-kawasaki-david-hiroshi/Práctica 2/Parte2/decorator_test.py
@@ -0,0 +1,62 @@
+import unittest
+
+from decorator import *
+
+class DecoratorTest(unittest.TestCase):
+
+    def test_character_without_equipment(self):
+        character = CharacterConcreteComponent(name='Luxor')
+        self.assertEqual(
+            character.equip(),
+            "Luxor equipment: Empty"
+        )
+
+    def test_character_with_armor(self):
+        character = CharacterConcreteComponent(name='Luxor')
+        armor = ArmorConcreteDecorator(character)
+        self.assertEqual(
+            armor.equip(),
+            "Luxor equipment:\nArmor: Yes"
+        )
+
+    def test_character_with_sword(self):
+        character = CharacterConcreteComponent(name='Luxor')
+        sword = SwordConcreteDecorator(character)
+        self.assertEqual(
+            sword.equip(),
+            "Luxor equipment:\nSword: Yes"
+        )
+    
+    def test_character_with_armor_and_sword(self):
+        character = CharacterConcreteComponent(name='Luxor')
+        armor = ArmorConcreteDecorator(character)
+        sword = SwordConcreteDecorator(armor)
+        self.assertEqual(
+            sword.equip(),
+            "Luxor equipment:\nArmor: Yes\nSword: Yes"
+        )
+
+    def test_character_with_armor_sword_and_ring(self):
+        character = CharacterConcreteComponent(name='Luxor')
+        armor = ArmorConcreteDecorator(character)
+        sword = SwordConcreteDecorator(armor)
+        ring = RingConcreteDecorator(sword)
+        self.assertEqual(
+            ring.equip(),
+            "Luxor equipment:\nArmor: Yes\nSword: Yes\nRing: Yes"
+        )
+
+    def test_character_with_armor_sword_ring_and_shield(self):
+        character = CharacterConcreteComponent(name='Luxor')
+        armor = ArmorConcreteDecorator(character)
+        sword = SwordConcreteDecorator(armor)
+        ring = RingConcreteDecorator(sword)
+        shield = ShieldConcreteDecorator(ring)
+        self.assertEqual(
+            shield.equip(),
+            "Luxor equipment:\nArmor: Yes\nSword: Yes\nRing: Yes\nShield: Yes"
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
