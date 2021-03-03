a6fb38d299957ad1fb1cda2de6503ed987b7c6fc
Angel Santiago Jaime Zavala <asjz2892@gmail.com>
2021-03-02 18:59:47 -0600

    Agrega solución parcial para práctica 2

---
+++ Ene-Jun-2021/Prácticas/1er Parcial/Práctica 2/decorator.py
@@ -0,0 +1,36 @@
+import abc
+
+# Component
+class Component(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def equip(self):
+        pass
+    
+# Concrete Component
+class CharacterConcreteComponent(Component):
+    def __init__(self, **args) -> None:
+        self._name = args.get('name', 'X')
+
+    def equip(self) -> str:
+        return str(self)
+    
+    def __str__(self) -> str:
+        return f"{self._name} equipment: Empty"
+
+# Base Decorator
+class BaseDecorator(Component):
+    def __init__(self, component: Component):
+        self._component = component
+
+    def equip(self):
+        return self._component.equip()
+
+# Concrete Decorator A
+class ArmorConcreteDecorator(BaseDecorator):
+    def equip(self):
+        return f"{super().equip().replace(' Empty', '')}\nArmor: Yes"
+
+# Concrete Decorator B
+class SwordConcreteDecorator(BaseDecorator):
+    def equip(self):
+        return f"{super().equip().replace(' Empty', '')}\nSword: Yes"
