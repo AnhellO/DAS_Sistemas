defaa3789212356889bc19c19b7d43c20d6d9757
Rephyroth <57422543+Rephyroth@users.noreply.github.com>
2021-03-16 19:43:56 -0600

    commit update decorator_native

--- Ene-Jun-2021/flores-fernandez-fernando/decorator_native.py
+++ Ene-Jun-2021/flores-fernandez-fernando/decorator_native.py
@@ -1,27 +1,65 @@
-def CharacterArmorDecorator(function):
-    def wrapper(name):
-        return f"{function(name)} Armor: Yes"
-    return wrapper
-        
-@CharacterArmorDecorator
-def CharacterConcreteComponent(name):
-    
-    def equip():
-        return f"{name} equipment: Empty"
-    
-    return equip  
-  
+import abc
+
+#inteface Component
+
+class ICharacterComponent(metaclass=abc.ABCMeta):
+
+    def __init__(self, name):
+        self.name = name
+
+    @abc.abstractmethod
+    def equip(self):
+        pass
+
+# Concrete Component
+class CharacterConcreteComponent(ICharacterComponent):
+    def equip(self):
+        return "Luxor equipment: Empty"
+
+
+# Base Decorator
+class CharacterDecorator(ICharacterComponent):
+    def __init__(self, ICharacterComponent: ICharacterComponent):
+        self._wrapper = ICharacterComponent
+    _wrapper: ICharacterComponent = None
+    w = _wrapper
+
+    def BaseDecorator(self, w: ICharacterComponent):
+        return self._wrapper
+
+    def equip(self):
+        return self._wrapper.equip()
+
+
+class ArmorConcreteDecorator(CharacterDecorator):
+    def __init__(self, decorator: CharacterDecorator):
+        self._decorator = decorator
+
+    def equip(self):  
+        if ( " Empty" in self._decorator.equip()):
+            return f"{self._decorator.equip()[0:-6]}\nArmor: Yes"
+        return f"{self._decorator.equip()}\nArmor: Yes"
+
+
+class SwordConcreteDecorator(CharacterDecorator):
+    def __init__(self, decorator: CharacterDecorator):
+        self.decorator = decorator
+
+    def equip(self):
+        if ( " Empty" in self._decorator.equip()):
+            return f"{self._decorator.equip()[0:-6]}\nSword: Yes"
+        return f"{self._decorator.equip()}\nSword: Yes"
+
+
+
 def main():
-    character = CharacterConcreteComponent("Luxor")
+    character = CharacterConcreteComponent(name = "Luxor")
+    armor = ArmorConcreteDecorator(character)
+    p = armor.equip()
+    
    
-    print(character())
-    
-if __name__ == '__main__':
-    main()    
+    print(p)
 
-
-        
-    
-    
-    
+if __name__ == "__main__":
+    main()
     
