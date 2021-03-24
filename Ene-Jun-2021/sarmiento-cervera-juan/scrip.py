dab807e6cd5031781bf3650e9fa1754fdc0363b8
black994 <jp9496@hotmail.com>
2021-03-23 21:48:55 -0600

    Remover Archivos

--- Ene-Jun-2021/sarmiento-cervera-juan/scrip.py
+++
@@ -1,5 +0,0 @@
-Python 3.8.7 (tags/v3.8.7:6503f05, Dec 21 2020, 17:59:51) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license" for more information.
->>> print ("Hola Clase de Desarollo y Arquitectura")
-Hola Clase de Desarollo y Arquitectura
->>> print("ya es viernes")
