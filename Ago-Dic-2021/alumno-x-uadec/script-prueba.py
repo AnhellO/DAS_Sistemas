548c9667992945111150a492e32ea3082a97c7a1
jarmarj <jarmarj_first@hotmail.com>
2021-08-23 19:50:36 -0500

    Agregando script de pyhton prueba

---
+++ Ago-Dic-2021/alumno-x-uadec/script-prueba.py
@@ -0,0 +1,30 @@
+#Hola Mundo!
+print('Hola clase! :D')
+
+#Operaciones aritmeticas 
+print(5+5)
+print(10- -5)
+print(2*4)
+print(10/3)
+print(6%2)
+print(2**5)
+print(10//5)
+
+#Variables
+x=10
+mi_string='DAS'
+un_string_de_multiples_lineas='''
+!Hola!
+    Estoy en 
+    varias lineas
+    '''
+
+print(x)
+print(mi_string)
+print(un_string_de_multiples_lineas.strip())
+
+#Listas
+mi_lista=[1, 2, 3]
+print(mi_lista)
+mi_lista.reverse()
+print(mi_lista)
