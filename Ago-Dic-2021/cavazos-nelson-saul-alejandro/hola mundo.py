ff19682f0b48ce55046a3413799438be11fd9886
SaulCN <60274195+SaulCN@users.noreply.github.com>
2021-08-28 12:54:53 -0500

    agregando practica de clase

---
+++ Ago-Dic-2021/cavazos-nelson-saul-alejandro/hola mundo.py
@@ -0,0 +1,31 @@
+# hola mundo
+print ('hola clase! :D')
+
+#operaciones aritmeticas
+print (5 + 5)
+print (10 - -5)
+print (2 * 4)
+print (10 / 3)
+print (7 % 2)
+print (2 ** 5)
+print (10 // 3)
+
+#variables 
+x=10
+mi_string= 'das'
+un_string_demultiples_lineas = '''
+     ¡hola!
+    estoy en varias 
+     lineas
+'''
+print (x)
+print(mi_string)
+print(un_string_demultiples_lineas.strip())
+print(mi_string.lower())
+
+mi_lista=[1, 2, 3]
+print(mi_lista)
+mi_lista.reverse()
+print(mi_lista)
+otra_lista=[1, 2, 3, True, False, 'hola', ['mundo']]
+print(otra_lista)
