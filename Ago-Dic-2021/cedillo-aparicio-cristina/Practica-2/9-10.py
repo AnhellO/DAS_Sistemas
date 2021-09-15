abf9fc520aaa0642e208eae52cffe5afffb9b0e1
cristyCA <cristy-ca98>
2021-09-14 19:55:25 -0500

    Añadiendo tarea sobre el libro Python Crash Course

---
+++ Ago-Dic-2021/cedillo-aparicio-cristina/Practica-2/9-10.py
@@ -0,0 +1,5 @@
+from restaurant import Restaurante
+print('\n-----------------9-10-------------------------')
+restaurant = Restaurante('Escolleras', 'comida de mar')
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
