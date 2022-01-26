ed32be4e7b16ac14b271748e69f2d6991a9f9514
black994 <jp9496@hotmail.com>
2022-01-25 20:52:06 -0600

    espero se este subiendo en donde es

---
+++ Ene-Jun-2022/juan-pablo-sarmiento-cervera/practica1/prac1.py
@@ -0,0 +1,16 @@
+# Esto es un comentario de python
+
+#Declaramos Variable
+x = 5
+
+#imprimo mi variable
+print (x)
+
+#Operaciones aritmeticas con la variable
+print("x + 5 = ",x + 5)
+print("x - 5 = ",x - 5)
+print("x * 5 = ",x * 5)
+print("x / 5 = ",x / 5)
+print("x % 5 = ",x % 5)
+print("x // 5 = ",x // 5)
+print("x ** 5 = ",x ** 5)
