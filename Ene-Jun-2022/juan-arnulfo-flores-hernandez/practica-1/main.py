02fc2d6d3286e576c0b61dcd0b892ee9a72e242e
juan1899flores <juanflores1899@gmail.com>
2022-01-25 20:41:52 -0600

    actualizando practica 1

--- Ene-Jun-2022/juan-arnulfo-flores-hernandez/practica-1/main.py
+++ Ene-Jun-2022/juan-arnulfo-flores-hernandez/practica-1/main.py
@@ -5,4 +5,15 @@
 x=5
 #manipulo mi variable
 
-print(x)
+print("x = ",x)
+
+#operaciones aritmeticas con mi variable
+
+print("x+5=",x+5)
+print("x-5=",x-5)
+print("x*5=",x*5)
+print("x/5=",x/5)
+print("x%5=",x%5)
+print("x//5=",x//5)
+print("x**5=",x**5)
+
