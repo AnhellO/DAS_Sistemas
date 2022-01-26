2f0923ed481affb561166d44b6ab5cc7f6a51415
Victor835xd <60279237+Victor835xd@users.noreply.github.com>
2022-01-25 21:43:22 -0600

    Agrego practica 1

---
+++ Ene-Jun-2022/victor-geronimo-de-leon-cuellar/Practica1.py
@@ -0,0 +1,12 @@
+#Crear una variable
+numero=5
+#imprimir la variable
+print(numero)
+#Realizar Operaciones
+print("5 + x = ", numero + 5)
+print("5 - x = ", numero - 5)
+print("5 * x = ", numero * 5)
+print("5 / x = ", numero / 5)
+print("5 % x = ", numero % 5)
+print("5 // x = ", numero + 5)
+print("5 ** x = ", numero ** 5)
