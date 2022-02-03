634b5a0aa2cd78471fad8d2e73f7a827022cdc92
Damian Rafael Hernandez <37432315+DamianRafael@users.noreply.github.com>
2022-02-03 10:59:14 -0600

    Add files via upload

---
+++ Ene-Jun-2022/damian-rafael-hernandez-saucedo/Practica-1/main.py
@@ -0,0 +1,21 @@
+
+#declaracion 
+x = 5
+
+#impresion 
+print ("x = ",x)
+
+#Impresion 
+print ("x + 5 = ",x + 5)
+
+print ("x - 5 = ",x - 5)
+
+print ("x / 5 = ",x / 5)
+
+print ("x * 5 = ",x * 5)
+
+print ("x // 5 = ",x // 5)
+
+print ("x ^ 5 = ",x ^ 5)
+
+print ("x % 5 = ",x % 5)
