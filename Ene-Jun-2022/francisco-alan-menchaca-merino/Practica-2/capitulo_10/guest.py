2a32e09fef0935511c860c2e332fb96e96190ee1
Fco. Alan Menchaca M <alanmenchaca22@outlook.com>
2022-02-13 23:53:35 -0600

    Removing files...

--- Ene-Jun-2022/francisco-alan-menchaca-merino/Practica-2/capitulo_10/guest.py
+++
@@ -1,9 +0,0 @@
-""" 10-3. Guest: Write a program that prompts the user for their name.
-When they respond, write their name to a file called guest.txt. """
-
-filename = "guest.txt"
-user_name = input("What is your name?: ")
-
-with open(filename, "w") as file_object:
-    file_object.write(user_name)
-print(f"Name saved on the text file \"{filename}\" ")
--- Ene-Jun-2022/francisco-alan-menchaca-merino/Practica-2/capitulo_8/printing_sandwiches.py
+++
@@ -1,4 +0,0 @@
-def make_sandwich(*toppings):
-    print("\nMaking a sandwich with the following toppings:")
-    for topping in toppings:
-        print("- " + topping)
