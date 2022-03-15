b5221d6c03d15e963c15d3a06e441afc7afea536
black994 <jp9496@hotmail.com>
2022-03-15 16:42:07 -0600

    Practica 5

---
+++ Ene-Jun-2022/juan-pablo-sarmiento-cervera/práctica-5/ejercicio-3.py
@@ -0,0 +1,17 @@
+import abc
+from abc import ABC, abstractmethod
+
+class A(ABC):
+    def print_function(self):
+        print("Abstract Base Class")
+
+
+class SubClassB(A):
+
+    def print_function(self):
+        A.print_function(self)
+        print("Subclass")
+
+if __name__ == "__main__":
+    B = SubClassB()
+    B.print_function()
