01791b061fc4439e568e36cb72dbc424e101686e
AlfonsoLoopez <92704004+AlfonsoLoopez@users.noreply.github.com>
2022-03-21 00:04:12 -0600

    Delete Ene-Jun-2022/alfonso-israel-lopez-garza/práctica-2 directory

--- Ene-Jun-2022/alfonso-israel-lopez-garza/práctica-2/Capítulo-8/Cap8-3_T-Shirt.py
+++
@@ -1,6 +0,0 @@
-def make_shirt(size, message):
-    '''Muestra el tamaño de la playera, junto con el texto impreso en ella.'''
-    print(f"El tamaño de la playera es: {size} y el texto en ella es: {message}")
-
-make_shirt('Chica','Felicidades') #Llamado de la función enviando Positional Arguments
-make_shirt(size = 'Grande', message = 'Hasta luego!') #Llamando a la función make_shirt usando Keyword arguments
